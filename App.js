28783e8d312f075808a5d974ef24320ab4ecc509
Roman <romeenice@gmail.com>
2023-02-12 15:49:08 +0200

    starting

--- App.js
+++ App.js
@@ -1,23 +1,7 @@
 import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.dummyText}>hello world</Text>
-      <Button title="click me" />
-    </View>
-  );
+  return <View></View>;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  dummyText: {
-    color: "red",
-    fontSize: 20,
-  },
-});
+const styles = StyleSheet.create({});
