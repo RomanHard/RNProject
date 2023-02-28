c1cdbadf3106a99464d5c85cc7bfa5166833338a
Roman <romeenice@gmail.com>
2023-03-01 00:32:51 +0200

    API_KEY

--- API/KEY.js
+++ API/KEY.js
@@ -1,3 +1,5 @@
 refs = {
   API_KAY: AIzaSyAmDlJfXKXDv7 - IiWyQsLMP93fE4LWuFKk,
 };
+
+export default refs;
--- API/youtube.js
+++ API/youtube.js
@@ -1,22 +1,25 @@
-// const handleAddVideo = async () => {
-//   try {
-//     // Запит на доступ до відео на ютуб
-//     const { type, token } = await Google.logInAsync({
-//       iosClientId: "your-ios-client-id",
-//       androidClientId: "your-android-client-id",
-//       scopes: ["https://www.googleapis.com/auth/youtube.force-ssl"],
-//     });
+import { Google } from "expo";
+import { Refs } from "./KEY";
 
-//     if (type === "success") {
-//       // Доступ було надано
-//       console.log(token);
-//       // Виконати запит на доступ до відео за допомогою YouTube Data API
-//       // ...
-//     } else {
-//       // Доступ не було надано
-//       console.log("Authentication failed");
-//     }
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
+const handleAddVideo = async () => {
+  try {
+    // Запит на доступ до відео на ютуб
+    const { type, token } = await Google.logInAsync({
+      iosClientId: "your-ios-client-id",
+      androidClientId: "your-android-client-id",
+      scopes: ["https://www.googleapis.com/auth/youtube.force-ssl"],
+    });
+
+    if (type === "success") {
+      // Доступ було надано
+      console.log(token);
+      // Виконати запит на доступ до відео за допомогою YouTube Data API
+      // ...
+    } else {
+      // Доступ не було надано
+      console.log("Authentication failed");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
